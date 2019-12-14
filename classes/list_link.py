a48ea227bf228cc997420c07034d1e85717a286c
François Sevestre <francois.sevestre.35@gmail.com>
2019-12-14 23:43:34 +0100

    Creation of a new object that links LDTs

---
+++ classes/list_link.py
@@ -0,0 +1,13 @@
+"""
+Contains ListLink class.
+"""
+
+#imports
+
+#main
+class ListLink():
+    """An object present in a list, that can switch the current main_list to another\\
+    or add its content to the current list (by reading the corresponding file).\\
+    In the first case, the linked list is virtualy contained in the list \\
+    and a link is present a the beginning of it.\\
+    In the second case, the linked list is a file store in the sauvegardes directory"""
