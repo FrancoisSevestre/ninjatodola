8a289e39c506731eb69d0d1b0eeb4205cdb580f2
François Sevestre <francois.sevestre.35@gmail.com>
2019-12-14 23:22:47 +0100

    ignoring no essential files.

--- fonctions/list_dir_dirs_files.py
+++
@@ -1,9 +0,0 @@
-from os import listdir, path
-
-def list_dir_dirs_files(path_to_file):
-    """Reads the path location and returns a tuple of dirs and files contained in it."""
-    files = [f for f in listdir(path_to_file) if path.isfile(path.join(path_to_file, f))]
-    dirs = [d for d in listdir(path_to_file) if path.isdir(path.join(path_to_file, d))]
-    output = (dirs, files)
-    return output
-
--- scripts/monter_son.sh
+++
@@ -1,4 +0,0 @@
-volume=`amixer sget Master|grep %|cut -d "[" -f 2|cut -d "%" -f 1`
-let "volume += 10"
-amixer sset Master $volume%
-
